588a4b5641a93a5c86d89ba18781bdd27e299a06
Abhishek Singh <abhishek404singh@gmail.com>
2022-03-08 01:29:15 +0530

    updated

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ async function showTags(){
     showData.innerHTML = details
 
 };
-showTags()
+// showTags()
 
 async function showVirals(){
     let res = await fetch(
@@ -56,5 +56,48 @@ async function showVirals(){
    
    };
 
-   showVirals()
+//    showVirals()
+
+
+   //debouncing
+   var timer
+   var appendDataDiv = document.getElementById("list")
+
+   function appendData(data) {
+       if(data.results.length>0){
+           appendDataDiv.style.display="block"
+       }
+       console.log(data.results)
+       data.results.filter((el,ind)=>ind<20).map(el=>{
+           let para = document.createElement("p")
+           para.innerHTML=el.user.name
+           appendDataDiv.append(para)
+       })
+   }
+
+   async function searchData(keyword){
+       let res = await fetch(`https://api.unsplash.com/search/photos?query=${keyword}&client_id=ERaqzjfKJiqyDoOTa3WsgaOiBujjJLtUSIDSB2CtfW4`)
+       const data = await res.json()
+         return data
+   }
+
+   async function deb(keyword){
+       if(keyword===""){
+           appendDataDiv.style.display="none"
+       }
+        const data = await searchData(keyword)
+        appendData(data)
+    }
+    
+    function debounce(deb,delay){
+        appendDataDiv.innerHTML=""
+        var keyword = document.getElementById("input").value
+        if(timer){
+            clearTimeout(timer)
+        }
+       timer = setTimeout(()=>{
+           deb(keyword)
+       },delay)
+   }
+//    debounce(deb,delay)
 
